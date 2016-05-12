e030b5071d84dd22a9733fa146e53dab90509a02
wuxb <a1072466135@163.com>
2016-05-12 15:58:34 +0800

    Change-Id: I3371b13abb54e7f4468d6029f0dda8db585bfe63 Signed-off-by: wuxb <a1072466135@163.com>

---
+++ MachineLearningPrac/src/algorithm/EncogML.java
@@ -0,0 +1,98 @@
+package algorithm;
+
+import java.io.File;
+import org.encog.ConsoleStatusReportable;
+import org.encog.ml.MLRegression;
+import org.encog.ml.data.versatile.NormalizationHelper;
+import org.encog.ml.data.versatile.VersatileMLDataSet;
+import org.encog.ml.data.versatile.columns.ColumnDefinition;
+import org.encog.ml.data.versatile.columns.ColumnType;
+import org.encog.ml.data.versatile.sources.CSVDataSource;
+import org.encog.ml.data.versatile.sources.VersatileDataSource;
+import org.encog.ml.factory.MLMethodFactory;
+import org.encog.ml.model.EncogModel;
+import org.encog.util.csv.CSVFormat;
+
+
+public class EncogML {
+	
+	private NormalizationHelper helper = null;
+	
+	public NormalizationHelper getHelper() {
+		return helper;
+	}
+	public void setHelper(NormalizationHelper helper) {
+		this.helper = helper;
+	}
+	public EncogML(){
+		NormalizationHelper helper = new NormalizationHelper();
+		this.helper = helper;
+	}
+
+	public MLRegression  train(String filePath) {
+			MLRegression bestMethod = null;
+			File irisFile = new File(filePath);
+			// Define the format of the data file.
+			// This area will change, depending on the columns and 
+			// format of the file that you are trying to model.
+			VersatileDataSource source = new CSVDataSource(irisFile, false,
+					CSVFormat.DECIMAL_POINT);
+			VersatileMLDataSet data = new VersatileMLDataSet(source);
+			data.defineSourceColumn("1", 0, ColumnType.continuous);
+			data.defineSourceColumn("2", 1, ColumnType.continuous);
+			data.defineSourceColumn("3", 2, ColumnType.continuous);
+			data.defineSourceColumn("4", 3, ColumnType.continuous);
+			data.defineSourceColumn("5", 4, ColumnType.continuous);
+			data.defineSourceColumn("6", 5, ColumnType.continuous);
+			data.defineSourceColumn("7", 6, ColumnType.continuous);
+			data.defineSourceColumn("8", 7, ColumnType.continuous);
+//			data.defineSourceColumn("9", 8, ColumnType.continuous);
+//			data.defineSourceColumn("10", 9, ColumnType.continuous);
+//			data.defineSourceColumn("11", 10, ColumnType.continuous);
+//			data.defineSourceColumn("12", 11, ColumnType.continuous);
+//			data.defineSourceColumn("13", 12, ColumnType.continuous);
+//			data.defineSourceColumn("14", 13, ColumnType.continuous);
+			// Define the column that we are trying to predict.
+			ColumnDefinition outputColumn = data.defineSourceColumn("species", 8,
+					ColumnType.nominal);
+			
+			// Analyze the data, determine the min/max/mean/sd of every column.
+			data.analyze();
+			
+			// Map the prediction column to the output of the model, and all
+			// other columns to the input.
+			data.defineSingleOutputOthersInput(outputColumn);
+			
+			// Create feedforward neural network as the model type. MLMethodFactory.TYPE_FEEDFORWARD.
+			// You could also other model types, such as:
+			// MLMethodFactory.SVM:  Support Vector Machine (SVM)
+			// MLMethodFactory.TYPE_RBFNETWORK: RBF Neural Network
+			// MLMethodFactor.TYPE_NEAT: NEAT Neural Network
+			// MLMethodFactor.TYPE_PNN: Probabilistic Neural Network
+			EncogModel model = new EncogModel(data);
+			model.selectMethod(data, MLMethodFactory.TYPE_FEEDFORWARD);
+			
+			// Send any output to the console.
+			model.setReport(new ConsoleStatusReportable());
+			
+			// Now normalize the data.  Encog will automatically determine the correct normalization
+			// type based on the model you chose in the last step.
+			data.normalize();
+			
+			// Hold back some data for a final validation.
+			// Shuffle the data into a random ordering.
+			// Use a seed of 1001 so that we always use the same holdback and will get more consistent results.
+			model.holdBackValidation(0.3, true, 1001);
+			
+			// Choose whatever is the default training type for this model.
+			model.selectTrainingType(data);
+			
+			// Use a 5-fold cross-validated train.  Return the best method found.
+			bestMethod = (MLRegression)model.crossvalidate(5, true);
+			return bestMethod;
+		} 
+	
+		public float predict(MLRegression bestMethod){
+			
+		}
+}
--- MachineLearningPrac/src/foundation/fileUtil/RWExcelFile.java
+++ MachineLearningPrac/src/foundation/fileUtil/RWExcelFile.java
@@ -1,5 +1,6 @@
 package foundation.fileUtil;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -7,9 +8,16 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-import jxl.*;   
-import jxl.Cell;
-import jxl.read.biff.BiffException;   
+import jxl.*;
+import jxl.format.UnderlineStyle;
+import jxl.read.biff.BiffException;
+import jxl.write.Label;
+import jxl.write.WritableCellFormat;
+import jxl.write.WritableFont;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
+import jxl.write.WriteException;
+import jxl.write.biff.RowsExceededException;   
 
 public class RWExcelFile {
 	/**
@@ -147,6 +155,202 @@ public class RWExcelFile {
 		 
 		 return flaTypeSet;
 	 }
+	 
+
+		/**
+		 * 生成一个Excel文件，若原文件存在，追加新的工作表
+		 * 
+		 * @param fileName
+		 *            ：Excel文件名
+		 * @param results
+		 *            ：写入的内容
+		 */
+		public static void write(String fileName,
+				List<List<DataCell>> results) {
+			WritableWorkbook wwb = null;
+			Workbook wb = null;
+			try {
+
+				File excelFile = new File(fileName);
+				if (excelFile.exists()) {
+					// 获得Excel工作薄(Workbook)对象
+					Workbook wb1 = Workbook.getWorkbook(excelFile);
+					// 打开一个文件的副本，并且指定数据写回到原文件
+					WritableWorkbook wwb1 = Workbook.createWorkbook(new File(
+							fileName), wb1);
+					wb = wb1;
+					wwb = wwb1;
+
+				} else {
+					// 使用Workbook类的工厂方法创建一个可写入的工作薄(Workbook)对象
+					WritableWorkbook wwb2 = Workbook.createWorkbook(excelFile);
+					wwb = wwb2;
+				}
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (BiffException e) {
+				e.printStackTrace();
+			}
+			if (wwb != null) {
+				// 创建一个可写入的工作表
+				// Workbook的createSheet方法有两个参数，第一个是工作表的名称，第二个是工作表在工作薄中的位置
+				//String fileNameWithouPath = FileNameUtil
+					//	.getFileNameInfo(excelfileName)[1];
+				int len = wwb.getSheets().length;
+				WritableSheet ws = wwb.createSheet(String.valueOf(len),
+						len);
+				int rowNum = results.size();
+				ws.setColumnView(0, 30); // 设置第1列的宽度
+				ws.setColumnView(1, 600); // 设置2列的宽度
+				ws.setColumnView(2, 600); // 设置3列的宽度
+				try {
+					for (int m = 0; m < 10; m++) {// 设置行的高度
+						ws.setRowView(m, 400);
+					}
+					ws.setRowView(10, 1200);
+				} catch (RowsExceededException e2) {
+
+					e2.printStackTrace();
+				}
+
+				WritableFont wf = new WritableFont(WritableFont.TIMES, 12,
+						WritableFont.BOLD, false, UnderlineStyle.NO_UNDERLINE,
+						jxl.format.Colour.CORAL); // 定义格式 字体 下划线 斜体 粗体 颜色
+				WritableCellFormat wcf = new WritableCellFormat(wf); // 单元格定义
+				// wcf.setBackground(jxl.format.Colour.BLACK); // 设置单元格的背景颜色
+				try {// 设置对齐方式
+					wcf.setAlignment(jxl.format.Alignment.LEFT);
+					wcf.setVerticalAlignment(jxl.format.VerticalAlignment.CENTRE);
+					wcf.setWrap(true);
+				} catch (WriteException e2) {
+					e2.printStackTrace();
+				}
+
+				// 下面开始添加单元格
+				for (int i = 0; i < rowNum; i++) {
+
+					List<DataCell> row = results.get(i);
+
+					int colSize = row.size();
+					for (int k = 0; k < colSize; k++) {
+						DataCell cell = row.get(k);
+						// // 这里需要注意的是，在Excel中，第一个参数表示列，第二个表示行
+						Label labelTitle = new Label(k, i, cell.getName(), wcf);
+						Label labelContent = new Label(k + 1, i,
+								cell.getValue(), wcf);
+						try {
+
+							// 将生成的单元格添加到工作表中
+							ws.addCell(labelTitle);
+							ws.addCell(labelContent);
+						} catch (RowsExceededException e) {
+							e.printStackTrace();
+						} catch (WriteException e) {
+							e.printStackTrace();
+						}
+
+					}
+
+				}
+			}
+
+			try {
+				// 从内存中写入文件中
+				wwb.write();
+				// 关闭资源，释放内存
+				wwb.close();
+				if (wb != null) {
+					wb.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (WriteException e) {
+				e.printStackTrace();
+			}
+		}
+
+		/**
+		 * 生成一个Excel文件,如果该文件存在，将覆盖原有文件内容
+		 * 
+		 * @param fileName
+		 *            要生成的Excel文件名
+		 */
+		public static void overideWrite(String fileName,
+				List<List<DataCell>> results) {
+			WritableWorkbook wwb = null;
+			try {
+				// 首先要使用Workbook类的工厂方法创建一个可写入的工作薄(Workbook)对象
+				wwb = Workbook.createWorkbook(new File(fileName));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			if (wwb != null) {
+				// 创建一个可写入的工作表
+				// Workbook的createSheet方法有两个参数，第一个是工作表的名称，第二个是工作表在工作薄中的位置
+				WritableSheet ws = wwb.createSheet("sheet1", 0);
+				int rowNum = results.size();
+				ws.setColumnView(0, 30); // 设置第1列的宽度
+				ws.setColumnView(1, 600); // 设置2列的宽度
+				try {
+					for (int m = 0; m < 10; m++) {// 设置行的高度
+						ws.setRowView(m, 400);
+					}
+					ws.setRowView(10, 1200);
+				} catch (RowsExceededException e2) {
+
+					e2.printStackTrace();
+				}
+
+				WritableFont wf = new WritableFont(WritableFont.TIMES, 12,
+						WritableFont.BOLD, false, UnderlineStyle.NO_UNDERLINE,
+						jxl.format.Colour.CORAL); // 定义格式 字体 下划线 斜体 粗体 颜色
+				WritableCellFormat wcf = new WritableCellFormat(wf); // 单元格定义
+				// wcf.setBackground(jxl.format.Colour.BLACK); // 设置单元格的背景颜色
+				try {// 设置对齐方式
+					wcf.setAlignment(jxl.format.Alignment.LEFT);
+					wcf.setVerticalAlignment(jxl.format.VerticalAlignment.CENTRE);
+					wcf.setWrap(true);
+				} catch (WriteException e2) {
+					e2.printStackTrace();
+				}
+
+				// 下面开始添加单元格
+				for (int i = 0; i < rowNum; i++) {
+					List<DataCell> row = results.get(i);
+					int colSize = row.size();
+					for (int k = 0; k < colSize; k++) {
+						DataCell cell = row.get(k);
+						// // 这里需要注意的是，在Excel中，第一个参数表示列，第二个表示行
+						Label labelTitle = new Label(k, i, cell.getName(), wcf);
+						Label labelContent = new Label(k + 1, i,
+								cell.getValue(), wcf);
+						try {
+							// 将生成的单元格添加到工作表中
+							ws.addCell(labelTitle);
+							ws.addCell(labelContent);
+						} catch (RowsExceededException e) {
+							e.printStackTrace();
+						} catch (WriteException e) {
+							e.printStackTrace();
+						}
+
+					}
+
+				}
+			}
+
+			try {
+				// 从内存中写入文件中
+				wwb.write();
+				// 关闭资源，释放内存
+				wwb.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (WriteException e) {
+				e.printStackTrace();
+			}
+		}
 	 
 	 public static void main(String[] args){
 		 readjxldefineColumn("D:\\a.xls","Sheet1",1);
